b45d396e6dcffa8b94cc89512b0d165d293f3d78
David Corrigan <davidcorrigan1@gmail.com>
2021-03-15 20:27:44 +0000

    Hot Fix for weather API when invalid coordinates

--- app/utils/weatherAPI.js
+++ app/utils/weatherAPI.js
@@ -5,19 +5,25 @@ const WeatherAPI = {
   getWeather: async function(longitude, latitude) {
     const lon = longitude;
     const lat = latitude;
-    const apiKey= process.env.api_key
+    const apiKey = process.env.api_key
+    try {
+      console.log(`Weather API Key = ${apiKey}`);
+      const weatherRequest = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+      let weather = {};
+      const response = await axios.get(weatherRequest);
+      if (response.status == 200) {
+        weather = response.data
+        console.log("Weather:" + weather.weather[0].description);
+      } else {
+        console.log("Could not find Weather at these coordinates")
+      };
 
-    console.log(`Weather API Key = ${apiKey}`);
-    const weatherRequest = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
-    let weather = {};
-    const response = await axios.get(weatherRequest)
-    if (response.status == 200) {
-      weather = response.data
-      console.log("Weather:" + weather.weather[0].description);
-    } else {
-      console.log("Could not find Weather at these coordinates")
-    };
-    return weather;
+      return weather;
+    } catch (err) {
+      console.log(err);
+      let weather = {};
+      return weather;
+    }
   }
 };
 
